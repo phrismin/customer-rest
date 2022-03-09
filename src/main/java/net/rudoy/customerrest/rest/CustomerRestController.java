494a242ff355e18c691f27a3f190df435e2a958d
Alexey Rudoy <rudenw01@gmail.com>
2022-03-09 20:56:31 +0300

    feat: added update and delete (Service)

--- src/main/java/net/rudoy/customerrest/rest/CustomerRestController.java
+++ src/main/java/net/rudoy/customerrest/rest/CustomerRestController.java
@@ -1,5 +1,6 @@
 package net.rudoy.customerrest.rest;
 
+import com.sun.net.httpserver.Headers;
 import net.rudoy.customerrest.model.Customer;
 import net.rudoy.customerrest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.util.UriComponentsBuilder;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/customers")
@@ -47,4 +48,37 @@ public class CustomerRestController {
         return new ResponseEntity<>(customer, headers, HttpStatus.OK);
     }
 
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> updateCustomer(@RequestBody @Valid Customer customer,
+                                                   UriComponentsBuilder builder) {
+
+        HttpHeaders headers = new HttpHeaders();
+
+        if (customer == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        customerService.save(customer);
+        return new ResponseEntity<>(customer, headers, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable Long id) {
+        if (id == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Customer customer = customerService.getById(id);
+
+        if (customer == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        customerService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+
+
+
+    }
 }
